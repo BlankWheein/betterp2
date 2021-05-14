c6c5522f940ae1f8fdb809685b111f8d62160972
blankwheein <noobs7410@live.dk>
2021-05-14 15:33:53 +0200

    asd

--- public/js/initmap.js
+++ public/js/initmap.js
@@ -6,100 +6,18 @@ let map;
 
 function initMap() {
   let origin = localStorage.getItem("origin");
-  let destination = localStorage.getItem("destination");
-  if ( origin == null) {
-    origin = "57.055194235611864, 9.90511334644895";
-  }
-  if (destination == null) {
-    destination = "57.055194235611864, 9.90511334644895";
-  }
-  console.log(origin, destination)
-  
+  let destination = localStorage.getItem("destination");  
     waypoints = {
       origin: origin,
       destination: destination,
       waypoint: []
     }
-
     localStorage.setItem("waypoints", JSON.stringify(waypoints));
     map = new google.maps.Map(document.getElementById("map"), {
       zoom: 15,
       disableDefaultUI: true,
     });
   
-    document.getElementById("submit").addEventListener("click", () => {
-      if (JSON.parse(localStorage.getItem("forceReview")) == null) {
-        localStorage.setItem("forceReview", false);
-      }
-      let path = []
-      var legs = directionsRenderer.getDirections().routes[0].legs;
-      for (i = 0; i < legs.length; i++) {
-        var steps = legs[i].steps;
-        for (j = 0; j < steps.length; j++) {
-          var nextSegment = steps[j].path;
-          for (k = 0; k < nextSegment.length; k++) {
-            path.push(nextSegment[k]);
-          }
-        }
-      }
-      let span = JSON.parse(localStorage.getItem("Span"));
-      for (const [key, value] of Object.entries(span)) {
-        span[key] = parseInt(value);
-      }
-      let body = {
-        lastpoint: false,
-        route: path,
-        route_raw: directionsRenderer.getDirections(),
-        events: JSON.parse(localStorage.getItem("place_events")),
-        truck: {
-          class: JSON.parse(localStorage.getItem("BridgeClassification")),
-          span: span,
-          width: parseFloat(localStorage.getItem("Width")),
-          height: parseFloat(localStorage.getItem("Height")),
-          length: parseFloat(localStorage.getItem("Length")),
-        },
-        waypoints: JSON.parse(localStorage.getItem("waypoints")),
-        forceReview: JSON.parse(localStorage.getItem("forceReview")),
-      }
-      FetchRetry("/get/routes", 1000, 5, {}, (data) => {
-        console.log(data.routes.approved);
-        for (let route of data.routes.approved) {
-          let path = route.data.route;
-          let line = createPolyline({path:path});
-          console.log(path, path.length)
-          body.lastpoint = resultPath = google.maps.geometry.poly.containsLocation(
-            new google.maps.LatLng(path[path.length - 1 ].lat, path[path.length - 1 ].lng),
-            line
-          )
-          if (body.lastpoint) {
-            console.log("Last point was true");
-            break;
-          }
-        }
-        fetch("/checkroute", {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json'
-            },
-          body: JSON.stringify(body)
-        }).then(data => data.json()).then(data => {
-          console.log(data);
-          if (data.status == 123) {
-            alert("Route was rejected! either apply for the permit again to force manual review or change the route");
-            localStorage.setItem("forceReview", true);
-          } else {
-            let uuid = JSON.parse(localStorage.getItem("uuid"));
-            uuid.push(data.uuid)
-            localStorage.setItem("uuid", JSON.stringify(uuid));
-            FetchRetry(`/get/approved/${data.uuid}`, 10000, 9999, {}, uuidApproved)
-            localStorage.setItem("forceReview", false);
-          }
-          
-      });
-        
-      })
-      
-  });
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer({
       draggable: false,
@@ -146,7 +64,79 @@ function initMap() {
     }
     
   }
-
+  document.getElementById("submit").addEventListener("click", () => {
+    if (JSON.parse(localStorage.getItem("forceReview")) == null) {
+      localStorage.setItem("forceReview", false);
+    }
+    let path = []
+    var legs = directionsRenderer.getDirections().routes[0].legs;
+    for (i = 0; i < legs.length; i++) {
+      var steps = legs[i].steps;
+      for (j = 0; j < steps.length; j++) {
+        var nextSegment = steps[j].path;
+        for (k = 0; k < nextSegment.length; k++) {
+          path.push(nextSegment[k]);
+        }
+      }
+    }
+    let span = JSON.parse(localStorage.getItem("Span"));
+    for (const [key, value] of Object.entries(span)) {
+      span[key] = parseInt(value);
+    }
+    let body = {
+      lastpoint: false,
+      route: path,
+      route_raw: directionsRenderer.getDirections(),
+      events: JSON.parse(localStorage.getItem("place_events")),
+      truck: {
+        class: JSON.parse(localStorage.getItem("BridgeClassification")),
+        span: span,
+        width: parseFloat(localStorage.getItem("Width")),
+        height: parseFloat(localStorage.getItem("Height")),
+        length: parseFloat(localStorage.getItem("Length")),
+      },
+      waypoints: JSON.parse(localStorage.getItem("waypoints")),
+      forceReview: JSON.parse(localStorage.getItem("forceReview")),
+    }
+    FetchRetry("/get/routes", 1000, 5, {}, (data) => {
+      console.log(data.routes.approved);
+      for (let route of data.routes.approved) {
+        let path = route.data.route;
+        let line = createPolyline({path:path});
+        console.log(path, path.length)
+        body.lastpoint = resultPath = google.maps.geometry.poly.containsLocation(
+          new google.maps.LatLng(path[path.length - 1 ].lat, path[path.length - 1 ].lng),
+          line
+        )
+        if (body.lastpoint) {
+          console.log("Last point was true");
+          break;
+        }
+      }
+      fetch("/checkroute", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(body)
+      }).then(data => data.json()).then(data => {
+        console.log(data);
+        if (data.status == 123) {
+          alert("Route was rejected! either apply for the permit again to force manual review or change the route");
+          localStorage.setItem("forceReview", true);
+        } else {
+          let uuid = JSON.parse(localStorage.getItem("uuid"));
+          uuid.push(data.uuid)
+          localStorage.setItem("uuid", JSON.stringify(uuid));
+          FetchRetry(`/get/approved/${data.uuid}`, 10000, 9999, {}, uuidApproved)
+          localStorage.setItem("forceReview", false);
+        }
+        
+    });
+      
+    })
+    
+});
 }
 
 /**
