7fc62b9d0076ae6e5c242163802f51be3b31ac84
D-Kuma-C <dberwa20@student.aau.dk>
2021-05-23 14:21:09 +0200

    ups2

--- public/js/helper_functions.js
+++ public/js/helper_functions.js
@@ -1,8 +1,8 @@
-function wait(delay){
+function wait(delay) {
   return new Promise((resolve) => setTimeout(resolve, delay));
 }
 
-function FetchRetry(url, delay, tries, fetchOptions = {}, callback){
+function FetchRetry(url, delay, tries, fetchOptions = {}, callback) {
   function onError(err) {
     triesLeft = tries - 1;
     console.log(`tries left ${triesLeft}`)
@@ -19,7 +19,7 @@ function FetchRetry(url, delay, tries, fetchOptions = {}, callback){
   }).catch(onError);
 }
 
-function createPolygon(data){
+function createPolygon(data) {
 
   data.name = data.name ?? "Not Specified";
   data.path = data.path ?? [];
@@ -47,53 +47,32 @@ function createPolygon(data){
   })
   return polygon;
 }
-function getHexCodeFromClass(c){
-  if (c == 0) {
-    return "#000000";
-  }
-  if (c > 0 && c <= 24){
-    return "#ffff33";
-  }
-  if (c > 24 && c <= 30){
-    return "#333399";
-  }
-  if (c > 30 && c <= 40){
-    return "#993333";
-  }
-  if (c > 40 && c <= 50){
-    return "#ffcc33";
-  }
-  if (c > 50 && c <= 60){
-    return "#993399";
-  }
-  if (c > 60 && c <= 70){
-    return "#33ccff";
-  }
-  if (c > 70 && c <= 80){
-    return "#ff6633";
-  }
-  if (c > 80 && c <= 90){
-    return "#66cc99";
-  }
-  if (c > 90 && c <= 100){
-    return "#336633";
-  }
-
+function getHexCodeFromClass(c) {
+  if (c == 0) { return "#000000"; }
+  if (c > 0 && c <= 24) { return "#ffff33"; }
+  if (c > 24 && c <= 30) { return "#333399"; }
+  if (c > 30 && c <= 40) { return "#993333"; }
+  if (c > 40 && c <= 50) { return "#ffcc33"; }
+  if (c > 50 && c <= 60) { return "#993399"; }
+  if (c > 60 && c <= 70) { return "#33ccff"; }
+  if (c > 70 && c <= 80) { return "#ff6633"; }
+  if (c > 80 && c <= 90) { return "#66cc99"; }
+  if (c > 90 && c <= 100) { return "#336633"; }
 }
 
-function drawRoads(map_){
+function drawRoads(map_) {
   FetchRetry("/get/routes", 2500, 10, {}, (data) => {
     let lines = []
     let lastColor = null;
     let pointColor = null;
 
-    for (let route of data.routes.approved){
+    for (let route of data.routes.approved) {
 
       console.log(route);
 
       path = []
-      for (let point of route.data.route){
-        if (data.routes.latlng.hasOwnProperty(`${point.lat} ${point.lng}`)){
+      for (let point of route.data.route) {
+        if (data.routes.latlng.hasOwnProperty(`${point.lat} ${point.lng}`)) {
           path.push(point);
           pointColor = getHexCodeFromClass(data.routes.latlng[`${point.lat} ${point.lng}`].class);
           if (lastColor != pointColor && pointColor != null) {
@@ -114,20 +93,20 @@ function drawRoads(map_){
         lines.push(poli);
       }
     }
-    for (let poly of lines){
+    for (let poly of lines) {
       poly.setMap(map_);
     }
   })
 }
 
-function loadVerticalMenu(){
+function loadVerticalMenu() {
   let menu = document.getElementById("vertical-menu");
   let links = [{ href: "/", name: "Hjem" }, { href: "TruckInput.html", name: "Opret vogntog" },
-               { href: "map.html", name: "Kort" }, { href: "review.html", name: "Til godkendelse" },
-               { href: "roadwork.html", name: "Vejarbejde" },
-               { href: "bridgeadder.html", name: "Tilføj bro" },
-               { href: "bridgeupdater.html", name: "Bro-værktøj" },
-               { href: "heatmap.html", name: "Heatmap" }];
+  { href: "map.html", name: "Kort" }, { href: "review.html", name: "Til godkendelse" },
+  { href: "roadwork.html", name: "Vejarbejde" },
+  { href: "bridgeadder.html", name: "Tilføj bro" },
+  { href: "bridgeupdater.html", name: "Bro-værktøj" },
+  { href: "heatmap.html", name: "Heatmap" }];
   links.forEach(e => {
     let link = document.createElement("a");
     link.href = e.href;
@@ -135,7 +114,7 @@ function loadVerticalMenu(){
     menu.appendChild(link);
   })
 }
-function createPolyline(data){
+function createPolyline(data) {
   data.name = data.name ?? "Not Specified";
   data.path = data.path ?? [];
   data.class = data.class ?? 0;
